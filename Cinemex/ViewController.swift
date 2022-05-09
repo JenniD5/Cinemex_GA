37a3cf447f2529a8ee8b113e0e0d00db761682e2
José Trejo <joseatrejos>
2022-05-09 13:35:38 -0700

    Initial Commit

---
+++ Cinemex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cinemex
+//
+//  Created by Alumno on 5/9/22.
+//  Copyright © 2022 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
